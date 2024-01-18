4c5510ef18723b5529906774a578234310455304
Bruno <bruno.rangel@ammovarejo.com.br>
2024-01-18 20:05:10 -0300

    try uri

--- src/producao/infraestructure/database/mongo.module.ts
+++ src/producao/infraestructure/database/mongo.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { MONGO_URL } from 'src/config/env';
+import { MONGO_URI } from 'src/config/env';
+import { CategoriaSchema } from 'src/producao/core/schemas/categoria.schema';
+import { ItemSchema } from 'src/producao/core/schemas/item.schema';
+import { PedidoSchema } from 'src/producao/core/schemas/pedido.schema';
+import { ProdutoSchema } from 'src/producao/core/schemas/produto.schema';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(MONGO_URL),
-  ],
+  imports: [MongooseModule.forRoot(MONGO_URI),
+  MongooseModule.forFeature([
+    { name: 'Pedido', schema: PedidoSchema },
+    {name: 'Item', schema: ItemSchema},
+    {name: 'Categoria', schema: CategoriaSchema},
+    {name: 'Produto', schema: ProdutoSchema}
+  ])],
 })
-export class MongooseDbModule { 
-
-}
+export class MongooseDbModule { }
